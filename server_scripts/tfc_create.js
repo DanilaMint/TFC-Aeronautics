d9deaed1f75c53439f6dc51cabdc9880d0a6722c
DanilaKouzmenko <138717464+DanilaKouzmenko@users.noreply.github.com>
2026-06-27 00:38:52 +0400

    fix .gitignore

--- server_scripts/tfc_create.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes((event) => {
-    // deleting copper ingot to copper sheet
-    event.remove({ input: "tfc:metal/ingot/copper", type: "create:pressing" });
-
-    event.recipes.create.pressing(
-        "tfc:metal/sheet/copper",
-        "tfc:metal/double_ingot/copper",
-    );
-});
--- startup_scripts/tfc_create.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry("item", (event) => {});
-/*
-StartupEvents.modifyCreativeTab("misc", event => {
-  event.addAfter("", "tfc_aeronautics:copper_dust");
-});
-*/
